aa0514253446ebd9e384f03b19711ee90eced296
Bill Rawlinson <bill.rawlinson@gmail.com>
2020-03-25 18:50:09 -0400

    Update initiative.js

--- initiative.js
+++ initiative.js
@@ -1,7 +1,7 @@
-// player button:
+// player button: must be a token action or else the "tracker" part wont work.
 
-/em rolls a [[1d20+@{initiative_bonus|0}&{tracker}]] for initiative!
+/em rolls a [[1d20+@{selected|initiative_bonus|0}&{tracker}]] for initiative!
 
 
-// dm button for all npcs..
+// dm macro button for all npcs.. make it a token button for each access; plus you can roll PC init if they aren't paying attention.
 /emas @{selected|token_name} rolls a [[1d20+@{selected|initiative_bonus|0}&{tracker}]] for initiative!
