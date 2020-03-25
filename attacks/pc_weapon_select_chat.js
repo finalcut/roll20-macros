cc7204c0a2432df72bf3542a677bd8e1400a9b2d
Bill Rawlinson <bill.rawlinson@gmail.com>
2020-03-24 21:06:24 -0400

    Create pc_weapon_select_chat.js

---
+++ attacks/pc_weapon_select_chat.js
@@ -0,0 +1,9 @@
+/w "@{selected|character_name}"&{template:atk} {{desc=**Weapon attacks:**
+
+``[@{selected|repeating_attack_$0_atkname}](~selected|repeating_attack_$0_attack)``
+
+``[@{selected|repeating_attack_$1_atkname}](~selected|repeating_attack_$1_attack)``
+
+``[@{selected|repeating_attack_$2_atkname}](~selected|repeating_attack_$2_attack)``
+
+}}
