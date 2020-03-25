4317b2a5ca0eafe67fde2ba64011f1b94826c1d7
Bill Rawlinson <bill.rawlinson@gmail.com>
2020-03-25 18:45:27 -0400

    Create initiative.js

---
+++ initiative.js
@@ -0,0 +1,7 @@
+// player button:
+
+/em rolls a [[1d20+@{initiative_bonus|0}&{tracker}]] for initiative!
+
+
+// dm button for all npcs..
+/emas @{selected|token_name} rolls a [[1d20+@{selected|initiative_bonus|0}&{tracker}]] for initiative!
