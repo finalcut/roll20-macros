1712917a4bbabed8979c5a0b855bc5836091ce62
Bill Rawlinson <bill.rawlinson@gmail.com>
2020-03-25 20:35:49 -0400

    null proficiency bonus

--- attacks/pc_first_weapon.js
+++ attacks/pc_first_weapon.js
@@ -1,15 +1,14 @@
 ## PC VERSION:
 
 /em attacks at @{target|token_name} with their @{selected|repeating_attack_$0_atkname}!
-&{template:default} {{name=@{selected|repeating_attack_$0_atkname}}} {{attack= [[?{Attack Type|Standard, 1d20|Advantage, 2d20kh1|Disadvantage, 2d20kl1} + @{selected|repeating_attack_$0_atkprofflag} [Proficiency Bonus] + @{selected|repeating_attack_$0_atkattr_base} [Attribute Modifier]  + (@{selected|repeating_attack_$0_atkmagic}+0) [Attack Bonus]]] }} {{Damage= [[  @{selected|repeating_attack_$0_dmgbase}+@{selected|repeating_attack_$0_dmgattr} + (@{selected|repeating_attack_$0_atkmagic}+0) ]] @{selected|repeating_attack_$0_dmgtype}}}
+&{template:default} {{name=@{selected|repeating_attack_$0_atkname}}} {{attack= [[?{Attack Type|Standard, 1d20|Advantage, 2d20kh1|Disadvantage, 2d20kl1} + (@{selected|repeating_attack_$0_atkprofflag}+0) [Proficiency Bonus] + @{selected|repeating_attack_$0_atkattr_base} [Attribute Modifier]  + (@{selected|repeating_attack_$0_atkmagic}+0) [Attack Bonus]]] }} {{Damage= [[  @{selected|repeating_attack_$0_dmgbase}+@{selected|repeating_attack_$0_dmgattr} + (@{selected|repeating_attack_$0_atkmagic}+0) ]] @{selected|repeating_attack_$0_dmgtype}}}
 /fx splatter-blood @{target|token_id} @{selected|token_id} 
 
 
 ## DM VERSION
 
-
 /emas @{selected|token_name} attacks at @{target|token_name} with their @{selected|repeating_attack_$0_atkname}!
-&{template:default} {{name=@{selected|repeating_attack_$0_atkname}}} {{attack= [[?{Attack Type|Standard, 1d20|Advantage, 2d20kh1|Disadvantage, 2d20kl1} + @{selected|repeating_attack_$0_atkprofflag} [Proficiency Bonus] + @{selected|repeating_attack_$0_atkattr_base} [Attribute Modifier]  + (@{selected|repeating_attack_$0_atkmagic}+0) [Attack Bonus]]] }} {{Damage= [[@{selected|repeating_attack_$0_dmgbase}+@{selected|repeating_attack_$0_dmgattr} + (@{selected|repeating_attack_$0_atkmagic}+0)]] @{selected|repeating_attack_$0_dmgtype}}}
+&{template:default} {{name=@{selected|repeating_attack_$0_atkname}}} {{attack= [[?{Attack Type|Standard, 1d20|Advantage, 2d20kh1|Disadvantage, 2d20kl1} + (@{selected|repeating_attack_$0_atkprofflag}+0) [Proficiency Bonus] + @{selected|repeating_attack_$0_atkattr_base} [Attribute Modifier]  + (@{selected|repeating_attack_$0_atkmagic}+0) [Attack Bonus]]] }} {{Damage= [[@{selected|repeating_attack_$0_dmgbase}+@{selected|repeating_attack_$0_dmgattr} + (@{selected|repeating_attack_$0_atkmagic}+0)]] @{selected|repeating_attack_$0_dmgtype}}}
 /fx splatter-blood @{target|token_id} @{selected|token_id} 
 
 
