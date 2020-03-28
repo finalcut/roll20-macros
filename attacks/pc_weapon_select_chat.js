2306b9e5260f442e1607f0824d6bcdd4a9e4faa3
Bill Rawlinson <bill.rawlinson@gmail.com>
2020-03-28 15:01:03 -0400

    Update pc_weapon_select_chat.js

--- attacks/pc_weapon_select_chat.js
+++ attacks/pc_weapon_select_chat.js
@@ -1,9 +1,21 @@
-/w "@{selected|character_name}"&{template:atk} {{desc=**Weapon attacks:**
+/w "@{selected|character_name}"&{template:atk} {{desc=**Select an Action:**
 
 ``[@{selected|repeating_attack_$0_atkname}](~selected|repeating_attack_$0_attack)``
 
 ``[@{selected|repeating_attack_$1_atkname}](~selected|repeating_attack_$1_attack)``
 
 ``[@{selected|repeating_attack_$2_atkname}](~selected|repeating_attack_$2_attack)``
+
+``[@{selected|repeating_attack_$3_atkname}](~selected|repeating_attack_$3_attack)``
+
+``[@{selected|repeating_attack_$4_atkname}](~selected|repeating_attack_$4_attack)``
+
+``[@{selected|repeating_attack_$5_atkname}](~selected|repeating_attack_$5_attack)``
+
+``[@{selected|repeating_attack_$6_atkname}](~selected|repeating_attack_$6_attack)``
+
+``[@{selected|repeating_attack_$7_atkname}](~selected|repeating_attack_$7_attack)``
+
+``[@{selected|repeating_attack_$8_atkname}](~selected|repeating_attack_$8_attack)``
 
 }}
